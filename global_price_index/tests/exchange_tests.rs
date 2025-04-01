8afa2435fdff9c674a71c195a2f3605160e6ab95
lykimq <lykimq@gmail.com>
2025-04-01 19:59:32 +0200

    Tests: exchange for Kraken

--- global_price_index/tests/exchange_tests.rs
+++ global_price_index/tests/exchange_tests.rs
@@ -1,5 +1,5 @@
 use global_price_index::{
-    exchanges::{binance::BinanceExchange, Exchange},
+    exchanges::{binance::BinanceExchange, kraken::KrakenExchange, Exchange},
     error::Result
 };
 use std::time::SystemTime;
@@ -27,3 +27,25 @@ async fn test_binance_order_book_calculation() -> Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn test_kraken_order_book_calculation() -> Result<()>{
+    let exchange = KrakenExchange::new().await?;
+    let order_book = exchange.fetch_order_book().await?;
+
+    assert!(!order_book.bids.is_empty());
+    assert!(!order_book.asks.is_empty());
+
+    for [price, _] in order_book.bids.iter() {
+        assert!(price.parse::<f64>().is_ok());
+    }
+
+    for [price, _] in order_book.asks.iter() {
+        assert!(price.parse::<f64>().is_ok());
+    }
+
+    assert!(order_book.timestamp <= SystemTime::now());
+
+    Ok(())
+}
+
